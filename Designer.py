bf95e02dc5706b8da5906871fce9a2ecdef8bddb
joy13975 <joyyeh.tw@gmail.com>
2017-02-10 16:33:06 +0000

    Sorted out python environments a little and updated readme

--- Designer.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-class Designer:
-	__metaclass__ = abc.ABCMeta
-
-	@abc.abstractmethod
-	def design(self, spec):
-		'''Base design abstraction'''
---
+++ tests/TestPositiveControl.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import glob
+from Designer import *
+import Greedy
+from utils import *
+import time
+
+def main():
+    xDB     = readJSON('res/xDB.json')
+    bmDir 	= 'bm/l10'
+
+    designers = []
+    designers.append(Greedy.GreedyDesigner(xDB, 'maxHeavy'))
+    # MCDesigner
+    # ...
+
+    # Process all benchmarks
+    for jsonFile in glob.glob(bmDir + '/*.json'):
+    	spec = readJSON(jsonFile)
+        targetLen = len(spec['nodes'])
+
+    	for designer in designers:
+            designerName = designer.__class__.__name__
+            print 'Benchmarking {} on {}, target length={}'.format(
+                designerName, jsonFile, targetLen)
+
+            startTime = time.clock()
+            (nodes,shape,score,fRot) = designer.design(spec, targetLen)
+            print "{:.2f}s, score: {}".format(time.clock() - startTime, score)
+
+            if nodes == spec['nodes']:
+                print 'Pass'
+            else:
+                print 'Failed'
+                pauseCode()
+    
+            makePdbFromNodes(xDB, 
+                nodes, 
+                'res/centered_pdb/pair', 
+                jsonFile.replace('.json', '_' + designerName + '_PCT.pdb'),
+                fRot)
+            
+if __name__ =='__main__': safeExec(main)
