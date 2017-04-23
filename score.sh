2f84a864ee08bc492a6c963ecd7cfd814b68efa0
Joy <cy13308@bluegem2.cm.cluster>
2017-04-23 02:53:14 +0100

    Relaxed

--- score.sh
+++ score.sh
@@ -2,7 +2,7 @@
 
 trap "exit" INT
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
 	echo "Usage: score.sh <min_pdb> <native_pdb>"
 	exit
 fi
@@ -13,14 +13,19 @@ native="$2"
 scOutput="${native/\.pdb/_comp.sc}"
 
 local=${local:-"no"}
-variant=${variant:-"default"}
+variant=${variant:-"mpi"}
 release=${release:-"linuxgccrelease"}
 wrapper=${wrapper:-""}
 
-cmd="score.$variant.$release -overwrite -in:file:native $native -s $min -out:file:scorefile $scOutput"
+if [[ "$variant" == "mpi" ]]; then
+        wrapper=$wrapper" mpirun"
+fi
+
+cmd="$wrapper score.$variant.$release -overwrite -in:file:native $native -s $min -out:file:scorefile $scOutput"
 
 if [[ "$local" == "yes" ]]; then
-	$cmd
+	# $cmd
+	echo $cmd
 else
 	sbatch -A other -p cpu -N 1 --ntasks-per-node=1 --wrap="$cmd"
 fi
