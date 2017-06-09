a4ceb4a4b2bb4a48e609663270c1a69ecdaa1845
joy13975 <joyyeh.tw@gmail.com>
2017-06-09 20:47:39 +0100

    Still unable to exclude submodule elfin-db..

--- GA/.obj/core/Kabsch.d
+++
@@ -1,8 +0,0 @@
-.obj/./core/Kabsch.o: core/Kabsch.cpp core/Kabsch.hpp \
- core/../data/TypeDefs.hpp jutil/src/util.h \
- core/../data/PrimitiveShorthands.hpp core/../data/Geometry.hpp \
- core/../data/Gene.hpp core/../data/TypeDefs.hpp core/MathUtils.hpp \
- core/../input/JSONParser.hpp core/../input/json.hpp \
- core/../input/SpecParser.hpp core/../input/../data/TypeDefs.hpp \
- core/../input/DBParser.hpp core/../input/../data/PairRelationship.hpp \
- core/../input/../data/TypeDefs.hpp
