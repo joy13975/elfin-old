9fb82997cd502d3f9d6f1a3b18d5638569934b3a
joy13975 <joyyeh.tw@gmail.com>
2017-06-09 20:47:39 +0100

    Still unable to exclude submodule elfin-db..

--- GA/.obj/core/MathUtils.d
+++
@@ -1,4 +0,0 @@
-.obj/./core/MathUtils.o: core/MathUtils.cpp core/MathUtils.hpp \
- core/../data/TypeDefs.hpp jutil/src/util.h \
- core/../data/PrimitiveShorthands.hpp core/../data/Geometry.hpp \
- core/../data/Gene.hpp core/../data/TypeDefs.hpp
